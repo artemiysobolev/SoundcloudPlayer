e99a30759e63c9d8c08d02b7979e44f76d58ce44
artemiy <->
2020-05-20 23:41:13 +0300

    Create login view with outlets and actions

---
+++ SoundCloudPlayer/SoundCloudPlayer/Modules/Login/LoginView.swift
@@ -0,0 +1,21 @@
+//
+//	ViewController.swift
+// 	SoundCloudPlayer
+//
+
+import UIKit
+
+class LoginView: UIViewController {
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func signInButtonTapped(_ sender: UIButton) {
+    }
+    @IBAction func signUpButtonTapped(_ sender: UIButton) {
+    }
+}
