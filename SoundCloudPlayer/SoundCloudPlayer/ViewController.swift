4f91fde17430f8373d485d507bafdaf6dc414449
artemiy <->
2020-05-20 21:15:44 +0300

    Create Xcode project

---
+++ SoundCloudPlayer/SoundCloudPlayer/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//	ViewController.swift
+// 	SoundCloudPlayer
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
